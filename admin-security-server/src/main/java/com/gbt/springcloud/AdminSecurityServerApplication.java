51ca7708b6053ffdecdd33b9830a390877fda8f0
gaobotao <bbs-gaotao123@163.com>
2020-05-25 13:40:06 +0800

    Spring Cloud入门-Admin服务监控中心(Hoxton版本)

---
+++ admin-security-server/src/main/java/com/gbt/springcloud/AdminSecurityServerApplication.java
@@ -0,0 +1,20 @@
+package com.gbt.springcloud;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author botaogao
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSecurityServerApplication.class, args);
+    }
+
+}
